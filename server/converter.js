929bc72d1ebea586f5eae807e55a5b9291cb3b41
divyesh.bhalani <divyesh.bhalani@coruscate.ca>
2021-02-09 10:26:34 +0530

    asd

--- server/converter.js
+++ server/converter.js
@@ -1,7 +1,10 @@
 const acorn = require("acorn");
 const fs = require("fs");
 const walk = require("acorn-walk");
-const _ = require('lodash')
+const _ = require("lodash");
+const path = require("path");
+const e = require("cors");
+
 const converter = (req, resp) => {
   try {
     const body = req.body;
@@ -9,25 +12,29 @@ const converter = (req, resp) => {
     let temp = "";
     temp = fs.readFileSync(body.path + "/app.js");
     temp = acorn.parse(temp.toString());
-    _.forEach(temp.body, (t,i) => {
-      if(t.type==="VariableDeclaration"){
-        _.forEach(t.declarations,(d,j)=>{
-          if(d.init && d.init.callee && d.init.callee.name=== "require"){
-          _.forEach(d.init.arguments,(v,k)=>{
-            // console.log("Hi",v)
-            let recPath = body.path
-            if(_.startsWith(v.value,"./")){
-              recPath = recPath + v.value.split()
-            }
-          })
+    _.forEach(temp.body, (t, i) => {
+      if (t.type === "VariableDeclaration") {
+        _.forEach(t.declarations, (d, j) => {
+          if (d.init && d.init.callee && d.init.callee.name === "require") {
+            _.forEach(d.init.arguments, (v, k) => {
+              // console.log("Hi",v.value)
+              // let recPath = body.path;
+              let filePath = pathMaker(body.path, v.value);
+              if (filePath) {
+                console.log(
+                  "🚀 ~ file: converter.js ~ line 23 ~ _.forEach ~ filePath",
+                  filePathMaker(pathMaker(body.path, v.value))
+                );
+              }
+            });
           }
-        })
+        });
       }
     });
     // walk.simple(temp, {
-      // ExpressionStatement(node) {
-        // console.log("🚀 ~ file: converter.js ~ line 13 ~ Literal ~ node", node);
-      // },
+    // ExpressionStatement(node) {
+    // console.log("🚀 ~ file: converter.js ~ line 13 ~ Literal ~ node", node);
+    // },
     // });
     return resp.send({ data: temp });
     const data = acorn.parse();
@@ -79,4 +86,41 @@ const converter = (req, resp) => {
   }
 };
 
+const pathMaker = (basePath, requiredPath) => {
+  let absPath = "";
+  if (_.startsWith(requiredPath, "./")) {
+    let chunksOfBasePath = basePath.split("/");
+    let chunksOfRequiredPath = [];
+    _.map(requiredPath.split("/"), (p) => {
+      if (p === ".") {
+      } else if (p === "..") {
+        chunksOfBasePath.pop();
+      } else {
+        chunksOfRequiredPath.push(p);
+      }
+    });
+    _.map(chunksOfBasePath, (c) => {
+      if (c) {
+        absPath = absPath + "/" + c;
+      }
+    });
+    _.map(chunksOfRequiredPath, (c) => {
+      absPath = absPath + "/" + c;
+    });
+
+    return absPath;
+  }
+  return "";
+};
+const filePathMaker = (filePath) => {
+  if (fs.existsSync(filePath + ".js")) {
+    return filePath + ".js";
+  } else if (fs.existsSync(filePath + "/index.js")) {
+    return filePath + "/index.js";
+  } else {
+    return filePath;
+  }
+};
+
 module.exports = converter;
+filePathMaker;
